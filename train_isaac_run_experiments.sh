4952c398bc7dee13ec495f1b383e3c8fcb8849da
Jack Robert Lowry <jrl712@g3060.hyak.local>
2025-02-06 16:11:13 -0800

    updated a bug which sliced the hidden value instead of returning the most recent hidden value in IMP

--- train_isaac_run_experiments.sh
+++ train_isaac_run_experiments.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a arr=("networks/fast-rcnn", "networks/fast-rcnn-lstm")
+declare -a arr=("networks/imp-iterations/one" "networks/imp-iterations/two" "networks/imp-iterations/three")
 
 for i in "${arr[@]}"
 do
