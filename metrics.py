added fixes for f1, recall, precision metrics

metrics.py
@@ -1,32 +1,60 @@
 import sklearn
 import torch
 
-def f1_score(logits, gt):
-    pred_labels = torch.argmax(logits, dim=-1)
+def graph_f1_score(logits, gt, avg='macro'):
+    precision = graph_precision(logits, gt, avg=avg)
+    recall = graph_recall_at_k(logits, gt, k=1, avg=avg)
 
-    return sklearn.metrics.f1_score(
-        gt,
-        pred_labels,
-        average='macro'
-    )
+    f1_score =  (2*precision*recall)/(precision+recall)
 
-def recall_at_k(logits, gt, k):
+    return f1_score
+
+# logits: shape (B, num_nodes/edges, num_classes)
+# gt: shape (B, num_nodes/edges)
+def graph_recall_at_k(logits, gt, k, avg='macro'):
+    top_k_preds = torch.topk(logits, k, dim=-1).indices  # Shape: (n_samples, k)
     
-
-    top_k_preds = torch.topk(logits, k, dim=1).indices  # Shape: (n_samples, k)
-    
-    label_in_top_k = (top_k_preds == gt.unsqueeze(1)).any(dim=1)  # Shape: (n_samples,)
+    label_in_top_k = (top_k_preds == gt.unsqueeze(-1)).any(dim=-1)  # Shape: (n_samples,)
 
     true_positive_in_class_mat = torch.nn.functional.one_hot(gt)
-    num_occurences_per_class = torch.sum(true_positive_in_class_mat, dim=1)
+    #sum across all nodes/edges in graph
+    num_occurences_per_class = torch.sum(true_positive_in_class_mat, dim=1).to(torch.float)
+    num_occurences = num_occurences_per_class.sum(dim=-1)
     true_positive_in_class_mat[torch.bitwise_not(label_in_top_k)] = 0
-    true_positives_per_class = torch.sum(true_positive_in_class_mat, dim=1)
+    #sum across samples and all nodes/edges in samplesQ
+    true_positives_per_class = torch.sum(true_positive_in_class_mat, dim=1).to(torch.float)
+    true_positives = true_positives_per_class.sum(dim=-1)
     
-    recall_at_k_per_class = num_occurences_per_class/true_positives_per_class
-    
-    # Compute mean Recall@K across all samples
-    return recall_at_k_per_class.mean()
+    if avg == 'macro':
+        recall_at_k = true_positives/num_occurences
+        recall_at_k = recall_at_k.mean()
+    if avg == 'micro':
+        recall_at_k = true_positives.sum()/num_occurences.sum()
+    return recall_at_k
 
-def confusion_matrix(logits, gt, labels=None):
+def graph_precision(logits, gt, avg='macro'):
     pred_labels = torch.argmax(logits, dim=-1)
-    return sklearn.metrics.confusion_matrix(gt, pred_labels, labels)
+
+    false_positives_per_class_mat = torch.nn.functional.one_hot(pred_labels)
+    false_positives_per_class_mat[pred_labels == gt] = 0
+    #sum across samples and all nodes/edges in samples
+    false_positives_per_class = torch.sum(false_positives_per_class_mat, dim=1).to(torch.float)
+    false_positives  = false_positives_per_class.sum(dim=-1)
+
+    true_positive_in_class_mat = torch.nn.functional.one_hot(gt)
+    true_positive_in_class_mat[pred_labels != gt] = 0
+    #sum across samples and all nodes/edges in samples
+    true_positives_per_class = torch.sum(true_positive_in_class_mat, dim=1).to(torch.float)
+    true_positives = true_positives_per_class.sum(dim=-1)
+
+    if avg == 'macro':
+        precision = true_positives/(false_positives+true_positives)   
+        precision = precision.mean()
+    if avg == 'micro':
+        true_positives = true_positives.sum()
+        false_positives = false_positives.sum()
+        precision = true_positives/(false_positives+true_positives)   
+    return precision
+# def confusion_matrix(logits, gt, labels=None):
+#     pred_labels = torch.argmax(logits, dim=-1)
+#     return sklearn.metrics.confusion_matrix(gt, pred_labels, labels)

train_isaaclab.py
@@ -226,12 +226,12 @@ def train(device, config):
                             pos_loss = pos_loss_metric(relative_position, gt_pos)
 
                             pred_node_logits = nn.functional.softmax(node_labels, dim=-1)
-                            node_logits_total.append(pred_edge_logits.cpu())
+                            node_logits_total.append(pred_node_logits.cpu())
                             pred_edge_logits = nn.functional.softmax(edge_labels, dim=-1)
                             edge_logits_total.append(pred_edge_logits.cpu())
 
-                            node_gt_total.append(gt_node_label.cpu())
-                            edge_gt_total.append(gt_edge_label.cpu())
+                            node_gt_total.append(gt_node_label.cpu().to(torch.long))
+                            edge_gt_total.append(gt_edge_label.cpu().to(torch.long))
 
                             test_node_loss.append(node_loss)
                             test_edge_loss.append(edge_loss)
@@ -247,12 +247,12 @@ def train(device, config):
                         node_gt_total = torch.concat(node_gt_total)
                         edge_gt_total = torch.concat(edge_gt_total)
 
-                        node_f1 = metrics.f1_score(node_logits_total, node_gt_total)
-                        edge_f1 = metrics.f1_score(edge_logits_total, edge_gt_total)
-                        node_recall_at_5 = metrics.recall_at_k(node_logits_total, node_gt_total, 5)
-                        node_language_labels = [dataset.metadata["object_id_to_name"][idx] for idx in range(dataset.num_object_labels)]
-                        node_confusion_matrix = metrics.confusion_matrix(node_logits_total, node_gt_total, labels=node_language_labels)
-                        edge_confusion_matrix = metrics.confusion_matrix(edge_logits_total, edge_gt_total, labels=edge_language_labels)
+                        node_f1 = metrics.graph_f1_score(node_logits_total, node_gt_total)
+                        edge_f1 = metrics.graph_f1_score(edge_logits_total, edge_gt_total)
+                        node_recall_at_5 = metrics.graph_recall_at_k(node_logits_total, node_gt_total, 5)
+                        # node_language_labels = [dataset.metadata["object_id_to_name"][idx] for idx in range(dataset.num_object_labels)]
+                        # node_confusion_matrix = metrics.confusion_matrix(node_logits_total, node_gt_total, labels=node_language_labels)
+                        # edge_confusion_matrix = metrics.confusion_matrix(edge_logits_total, edge_gt_total, labels=edge_language_labels)
 
 
                 avg_loss = sum(losses)/len(losses)