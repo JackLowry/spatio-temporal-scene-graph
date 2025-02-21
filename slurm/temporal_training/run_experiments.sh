b2c417276d9d3e6d3b3ecb1ec6ce917e7a2118cc
Jack Lowry <jrl712@cs.washington.edu>
2025-02-21 11:04:57 -0800

    reorganized scripts for slurm training

---
+++ slurm/temporal_training/run_experiments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+declare -a arr=("../../networks/imp-iterations/one" "../../networks/imp-iterations/two" "../../networks/imp-iterations/three" "../../networks/fast-rcnn")
+
+for i in "${arr[@]}"
+do
+    sbatch train.slurm "$i"
+done
